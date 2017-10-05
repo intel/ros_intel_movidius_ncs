f2d71dab38552224c7b04c73db045a1620d4f39e
Lu, Ken <ken.lu@intel.com>
2017-10-05 11:17:04 +0800

    fix the missing conversion for intrinsics usage

--- movidius_ncs_lib/include/movidius_ncs_lib/tensor.h
+++ movidius_ncs_lib/include/movidius_ncs_lib/tensor.h
@@ -46,8 +46,10 @@ public:
     return 3 * height_ * width_ * sizeof(uint16_t);
   }
 
+#if !defined(__i386__) && !defined(__x86_64__)
   static void fp32tofp16(uint16_t* __restrict out, float in);
   static void fp16tofp32(float* __restrict out, uint16_t in);
+#endif
 
 private:
   std::vector<uint16_t> tensor_;
