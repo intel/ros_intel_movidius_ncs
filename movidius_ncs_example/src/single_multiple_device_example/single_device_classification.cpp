debf4ed42f5be3b9e63b5139661496e5266e69eb
chaoli2 <chao1.li@intel.com>
2018-03-28 09:16:46 +0800

    fine tune opencv display effect (#72)
    Signed-off-by: Chao Li <chao1.li@intel.com>

--- movidius_ncs_example/src/single_multiple_device_example/single_device_classification.cpp
+++ movidius_ncs_example/src/single_multiple_device_example/single_device_classification.cpp
@@ -104,6 +104,7 @@ int main(int argc, char** argv)
     {
       cv_bridge::CvImage cv_image;
       cv_image.image = cv::imread(images_path[dice()]);
+      cv::resize(cv_image.image, cv_image.image, cv::Size(640, 480));
       cv_image.encoding = "bgr8";
       int cnt = 0;
 
